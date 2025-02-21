3bc776bc86fbc454e03012b86ae32e095de75338
Atria <atriamoiedy13@gmail.com>
2025-02-21 11:03:34 -0800

    figma

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,27 @@
 import { defineConfig } from "vite";
 import tailwindcss from "@tailwindcss/vite";
+import autoprefixer from "autoprefixer";
+
 export default defineConfig({
   plugins: [tailwindcss()],
-  base: "./",  
+  css: {
+    postcss: {
+      plugins: [autoprefixer()],
+    },
+  },
+  base: "./",
   build: {
-    outDir: "dist",  
+    outDir: "dist",
     rollupOptions: {
       input: {
         main: "index.html",
-        about: "about.html", 
+        about: "about.html",
         contact: "pricing.html",
       },
     },
+  },
+  server: {
+    open: true,
+    port: 3000,
   },
 });
