d21112937ffcace36c4042888e60d47f3e9b94a3
Atria <atriamoiedy13@gmail.com>
2025-02-21 10:53:11 -0800

    figma

---
+++ vite.config.ts
@@ -0,0 +1,16 @@
+import { defineConfig } from "vite";
+import tailwindcss from "@tailwindcss/vite";
+export default defineConfig({
+  plugins: [tailwindcss()],
+  base: "./",  
+  build: {
+    outDir: "dist",  
+    rollupOptions: {
+      input: {
+        main: "index.html",
+        about: "about.html", 
+        contact: "pricing.html",
+      },
+    },
+  },
+});
